77ed7a99e86b9461decbc7af66e599146f9c6326
KacperBadek <k.badek2001@gmail.com>
2023-06-08 15:09:40 +0200

    Dodanie formularza opinii produktu, wyświetlanie opinii produktu

--- projekt/sklep_produkt.php
+++ projekt/sklep_produkt.php
@@ -26,18 +26,82 @@ if (!$result) {
 }
 
 if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-       echo 'Produkt: ' . $row['nazwa'].  ' ' . $row['cena'] . ' ' . $row["opis"] . ' ' . '<img src="zdjecia_produktow/' . $row['zdjecie'] . '" alt="Product Image">';
-    }
-} else echo "0 results";
+while ($row = $result->fetch_assoc()) {
 
-$db->close();
 ?>
 
+<div>
+    <div id="photo" style="float: left">
+        <?php
+        echo '<img src="zdjecia_produktow/' . $row['zdjecie'] . '" alt="Product Image">';
+        ?>
+    </div>
+
+    <div id="product_data" style="float: left">
+        <?php
+        echo $row['nazwa'] . "  <h2>" . $row['cena'] . " zł</h2><br>";
+        echo $row['opis'];
+        }
+        } else echo "0 results";
+        $db->close();
+        ?>
+        <br><br><br>
+        <form method="post">
+            <button type="submit" name="addBusket">DODAJ DO KOSZYKA</button>
+            <button type="submit" name="buyButton">KUP I ZAPŁAĆ</button>
+        </form>
+
+    </div>
+</div>
 
 
+<div id="userOpinion" style="clear: both">
+    <fieldset>
+        <legend>Dodaj opinie</legend>
+    <form method="post">
+      Ilość gwiazdek:  1:<input type="radio" name="rating">
+        2:<input type="radio" name="rating">
+        3:<input type="radio" name="rating">
+        4:<input type="radio" name="rating">
+        5:<input type="radio" name="rating"> <br>
+        Opinia: <br>
+        <textarea cols="30" rows="10" name="opinion"></textarea><br>
+        <button type="submit" name="opinionButton">Dodaj</button>
+    </fieldset>
+    </form>
+</div>
 
-<footer id="footer">
+<div id="listOfOpinions">
+    <h2>Opinie produktu</h2>
+
+    <?php
+    $db = new mysqli('localhost', 'root', '', 'sklep');
+    if ($db->connect_errno) {
+        die("Błąd połączenia z bazą danych!");
+    }
+
+    $query = "SELECT `liczba_gwiazdek`, `opinia`, `id_uzytkownik`, `data_wystawienia_opinii` FROM `opinie` WHERE `id_produkt` = '$id'";
+    $result = $db->query($query);
+
+    if (!$result) {
+        die("Błąd zapytania: " . $db->error);
+    }
+
+    echo "<ul>";
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo $row['id_uzytkownik'];
+            echo $row['liczba_gwiazdek'];
+            echo $row['data_wystawienia_opinii'];
+            echo $row['opinia'];
+        }
+    } else echo "Brak opinii";
+    echo "</ul>";
+    $db->close();
+    ?>
+</div>
+
+<footer id="footer" style="clear:both">
     <h3>All rights reserved by ©me</h3>
 </footer>
 
